e654e950037c491c3cb5bbb4b31e3adc994f1547
Thomas Daykin <thomas_daykin@yahoo.co.uk>
2025-08-28 12:28:13 +0100

    Moved Defs to own namespace

---
+++ Source/definitions/defs.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace TradingControl.definitions
+{
+    public class Trading : BuildableDef
+    {
+        public string Name
+        {
+            get
+            {
+                return "TradingSpot".Translate();
+            }
+        }
+            
+        public string Label;
+        public string ID;
+        
+
+        public GraphicData tradelocation;
+        public string texturepath;
+        public string graphic_type;
+
+        public string altitude;
+
+        public float flamability;
+        public int worktobuild;
+        
+        public bool ScatterOnMapGen;
+        public bool UseHP;
+
+        public string Category;
+        public string TickType;
+            
+    }
+
+}
