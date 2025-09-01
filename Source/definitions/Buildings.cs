0987241a480f34618fe2ef68fa522573a5c143d0
Thomas Daykin <7566941+Toakan@users.noreply.github.com>
2025-09-01 22:28:20 +0100

    Standardise all Building Defs and actions

---
+++ Source/definitions/Buildings.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using TradingControl.functions;
+using Verse;
+using Verse.AI.Group;
+
+namespace TradingControl.definitions
+{
+
+    public class MarketPlace : Building
+    {
+        public SharedActions Actions = new SharedActions();
+        private int _count = 0;
+
+        public MarketPlace()
+        {
+            Actions.CheckForPreviousSpot(this);
+        }
+
+        protected override void Tick()
+        {
+            base.Tick();
+            if (_count++ > 1)
+            {
+                _count = 0;
+                List<Lord> lords = Map.lordManager.lords;
+                Actions.GoToTradeSpot(Position, lords);
+            }
+        }
+    }
+
+
+    public class TradingSpot : Building
+    {
+        public SharedActions Actions = new SharedActions();
+        private int _count = 0;
+
+        public TradingSpot()
+        {
+            Actions.CheckForPreviousSpot(this);
+        }
+
+        protected override void Tick()
+        {
+            base.Tick();
+            if (_count++ > 1)
+            {
+                _count = 0;
+                List<Lord> lords = Map.lordManager.lords;
+                Actions.GoToTradeSpot(Position, lords);
+            }
+
+        }
+    }
+
+    public class DropSpotIndicator : Building
+    {
+        public SharedActions Actions = new SharedActions();
+        private int _count = 0;
+
+        public DropSpotIndicator()
+        {
+            Actions.CheckForPreviousSpot(this);
+        }
+    }
+}
+
